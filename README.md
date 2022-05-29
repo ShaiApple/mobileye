7d9bebdd5c97624f054214da87e6e5f632cacb66
Shaibremer <bremershai@gmail.com>
2022-05-29 15:12:30 +0300

    try in linux

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mobileye
+
+for Linux:
+python3 "cli_tool.py" --name Shai Bremer --dept DE --id 3582
--- mobileye/params.py
+++ mobileye/params.py
@@ -1,13 +1,13 @@
 from mobileye import os, Logger
 
-
 API = "https://dct-tasks-db.herokuapp.com/tasks"
 PERMISSIONS_FILE = open(os.path.join('source', 'auth', 'permissions.json'))
-LOGS_PATH = os.path.dirname(os.path.join('destination', 'logs'))
+LOGS_PATH = os.path.join('destination', 'logs')
 
 
 def print_to_console(message: str):
     print(message)
+
 
 def write_to_log(logger: Logger, message: str):
     if message:
