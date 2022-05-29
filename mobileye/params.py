352a33a609f5b42a309ec86ea2c8f379086b58d7
Shaibremer <bremershai@gmail.com>
2022-05-29 14:12:27 +0300

    add os method

--- mobileye/params.py
+++ mobileye/params.py
@@ -2,8 +2,8 @@ from mobileye import os, Logger
 
 
 API = "https://dct-tasks-db.herokuapp.com/tasks"
-PERMISSIONS_FILE = open("source\\auth\permissions.json")
-LOGS_PATH = "destination\logs"
+PERMISSIONS_FILE = open(os.path.join('source', 'auth', 'permissions.json'))
+LOGS_PATH = os.path.dirname(os.path.join('destination', 'logs'))
 
 
 def print_to_console(message: str):
