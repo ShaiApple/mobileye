c4817358185426611eaf09e1c793196871d84b9b
Shaibremer <bremershai@gmail.com>
2022-05-29 15:30:48 +0300

    create LOGS_PATH

--- mobileye/params.py
+++ mobileye/params.py
@@ -3,6 +3,9 @@ from mobileye import os, Logger
 API = "https://dct-tasks-db.herokuapp.com/tasks"
 PERMISSIONS_FILE = open(os.path.join('source', 'auth', 'permissions.json'))
 LOGS_PATH = os.path.join('destination', 'logs')
+# check if exist:
+if not os.path.isdir(LOGS_PATH):
+    os.makedirs(LOGS_PATH)
 
 
 def print_to_console(message: str):
